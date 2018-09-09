d9d0b565073d79d2f08bc69db9b0253a911b313c
Lucas Caramelo <lucascaramelo@gmail.com>
2018-09-09 15:48:47 -0300

    simplify dagger inject

--- app/src/main/java/fiap/com/br/lockeriot/LockerApplication.kt
+++ app/src/main/java/fiap/com/br/lockeriot/LockerApplication.kt
@@ -5,9 +5,7 @@ import android.app.Application
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasActivityInjector
 import fiap.com.br.lockeriot.dagger.DaggerApplicationComponent
-import fiap.com.br.lockeriot.dagger.module.AndroidModule
 import fiap.com.br.lockeriot.dagger.module.ApplicationModule
-import fiap.com.br.lockeriot.dagger.module.RetrofitModule
 import javax.inject.Inject
 
 class LockerApplication: Application(), HasActivityInjector {
